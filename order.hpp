869dff323182224ec44783d9c3935df80e5c709c
harisang <harisangelidakis@gmail.com>
2021-12-01 23:26:54 +0200

    minor stylistic updates

--- order.hpp
+++ order.hpp
@@ -19,8 +19,6 @@ class Order {
         boost::multiprecision::mpz_int sell_amount;
         boost::multiprecision::mpz_int buy_amount;
 
-
-
         // constructor that 
         Order(int _order_index, int _sell_token_index, int _buy_token_index, bool _is_sell_order, bool _is_liquidity_order, bool _is_partial_fill, boost::multiprecision::mpz_int _sell_amount, boost::multiprecision::mpz_int _buy_amount): order_index(_order_index),  sell_token_index(_sell_token_index), buy_token_index(_buy_token_index), is_sell_order(_is_sell_order), is_liquidity_order(_is_liquidity_order), is_partial_fill(_is_partial_fill), sell_amount(_sell_amount), buy_amount(_buy_amount) {
         }
