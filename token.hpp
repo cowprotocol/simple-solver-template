c4f3ad58a7a76959d9608edef6366fbf82d61d3b
harisang <harisangelidakis@gmail.com>
2021-12-01 23:59:29 +0200

    minor edits

--- token.hpp
+++ token.hpp
@@ -8,12 +8,13 @@
 
 class Token {
     public:
-        static int num_tokens;       // counts how many tokens have been created so far
-        static std::map<std::string, int> idx_tokens;    // maps the token name to an index
+        static int num_tokens;                              // counts how many tokens have been created so far
+        static std::map<std::string, int> idx_tokens;       // maps the token name to an index
         const std::string name;
         const std::string alias;
         const unsigned int decimals;
-        const int idx;      // the index of the current token
+        const int idx;                                      // the index of the current token
+
 
         // constructor that 
         Token(std::string _name, std::string _alias, unsigned int _decimals): name(_name),  alias(_alias), decimals(_decimals), idx(num_tokens++) {
