95dba8671a0436c7f5ff7dcff7d0068f7d215dcb
harisang <harisangelidakis@gmail.com>
2021-12-01 20:37:38 +0200

    Some preliminary code for instance given in a json file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "./json.hpp"
+#include "./order.hpp"
+#include "./token.hpp"
+//#include "./amm.hpp"
+#include "./aux.hpp"
+
+
+
+/************************************** Global variables that are defined as static in various classes *************************************/
+
+int Token::num_tokens{ 0 };                     // static integer variable of class Token, that counts number of tokens in our instance
+std::map<std::string, int> Token::idx_tokens;          // static map of class Token, that maps the address of the token to a unique index
+
+/*******************************************************************************************************************************************/
+
+
+
+
+
 int main()
 {
+
+/********************************************************** VARIABLE DECLARATIONS **********************************************************/
+
+    std::vector<Token> tokens;      // the vector containing all the tokens appearing in any order/AMM
+    std::vector<Order> orders;      // the vector containing all the "regular" orders
+    //std::vector<AMM> amms;          // the vector containing all the AMMs
+
+/*******************************************************************************************************************************************/
+
+
+    // Parsing the input
+    parse_json_file(tokens, orders, Token::num_tokens, Token::idx_tokens);
+
+    
+    // Just printing out the input
+    for (auto &i: tokens)
+        print_token(i);
+
+    for (auto &i: orders)
+        print_order(i);
+
+
+    // solve batch auction
+
+
+    // return solution
+
+
     return 0;
 }
 
--- token.hpp
+++ token.hpp
@@ -1,6 +1,30 @@
-class Token {
-    std::string name;
-    
-}
+#pragma once
 
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include <string>
+#include <map>
+
+class Token {
+    public:
+        static int num_tokens;       // counts how many tokens have been created so far
+        static std::map<std::string, int> idx_tokens;    // maps the token name to an index
+        const std::string name;
+        const std::string alias;
+        const unsigned int decimals;
+        const int idx;      // the index of the current token
+
+        // constructor that 
+        Token(std::string _name, std::string _alias, unsigned int _decimals): name(_name),  alias(_alias), decimals(_decimals), idx(num_tokens++) {
+        }
+};
+
+
+// Prints token t in a "nice" way
+void print_token(Token &t);
+
+
+
+#endif
 
